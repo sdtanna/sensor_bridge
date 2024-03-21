630e8d334f694daa05189b4ff61811105f30b6ae
Shyam Tanna <shyamtanna@Shyams-MacBook-Pro.local>
2024-03-21 18:32:14 -0400

    Add heart beat

--- socket_handler.py
+++ socket_handler.py
@@ -2,6 +2,8 @@ import logging
 import json
 import socketio
 import numpy as np
+import threading
+import time
 
 
 class socketHandeler():
@@ -12,17 +14,16 @@ class socketHandeler():
         self.sio = socketio.Client(reconnection=True, reconnection_attempts=0, reconnection_delay=1)
         self.connected = False
         self.sio.on('disconnect', self.disconnect)
-        self.sio.on('reconnect', self.reconnect)
-
-
-
-        
+        self.sio.on('reconnect', self.reconnect)    
+ 
     def init_socketIO(self):
         """ Connect to Socket.IO server """
         try:
             self.sio.connect(self.sio_url)
             self.connected = True
             self.logger.info(f"Connected to Socket.IO server at {self.sio_url}")
+            self.start_heartbeat(interval=5)  # Start sending heartbeats every 5 seconds
+
         except Exception as e:
             self.logger.info(f"Failed to connect to Socket.IO: {e}")
     
@@ -30,8 +31,6 @@ class socketHandeler():
         self.connected = False
         self.logger.info("Disconnected from Socket.IO server")
         self.logger.info("Attempting to reconnect...")
-
-
 
     def reconnect(self):
         self.connected = True
@@ -57,3 +56,18 @@ class socketHandeler():
             except Exception as e:
 
                 self.logger.error(f"Error sending data to Socket.IO: {e}")
+
+    def start_heartbeat(self, interval=5):
+        """Start sending heartbeat messages at the given interval in seconds."""
+        def heartbeat():
+            while self.connected:
+                try:
+                    self.sio.emit('heartbeat', {'ping': '1'})
+                    self.logger.info("Heartbeat sent")
+                except Exception as e:
+                    self.logger.error(f"Failed to send heartbeat: {e}")
+                    self.connected = False  # Assume disconnected and trigger reconnection logic
+                time.sleep(interval)
+                
+        # Start the heartbeat in a separate thread to prevent blocking
+        threading.Thread(target=heartbeat, daemon=True).start()
